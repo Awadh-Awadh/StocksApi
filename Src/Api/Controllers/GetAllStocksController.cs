dc7c1edc4b5504dac90c2da95a465de2f0b91910
Awadh-Awadh <awadhawadh84@yahoo.com>
2023-05-29 12:52:39 +0300

    configure serilog with console and seq as the main sinks

---
+++ Src/Api/Controllers/GetAllStocksController.cs
@@ -0,0 +1,23 @@
+using Microsoft.AspNetCore.Mvc;
+
+namespace Api.Controllers;
+
+[ApiController]
+[Route("api/[controller]")]
+public class GetAllStocksController : ControllerBase
+{
+    private readonly ILogger<GetAllStocksController> _logger;
+    public GetAllStocksController(ILogger<GetAllStocksController> logger)
+    {
+        _logger = logger;
+    }
+   
+    [HttpGet(Name = "GetStocks")]
+    public IActionResult GetAllStocks()
+    {
+       
+        
+        return Ok();
+    }
+
+}
--- Src/Api/Program.cs
+++ Src/Api/Program.cs
@@ -1,4 +1,9 @@
+using Infrastructure;
+using Microsoft.AspNetCore.HttpLogging;
+using Serilog;
+
 var builder = WebApplication.CreateBuilder(args);
+var configuration = builder.Configuration;
 
 // Add services to the container.
 
@@ -6,7 +11,29 @@ builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
+builder.Services.AddHttpLogging(options =>
+{
+    options.LoggingFields = HttpLoggingFields.All;
+});
+builder.Services.AddInfrastructureServices(configuration);
 
+// configure serilog
+builder.Host.UseSerilog((context, provider, config) =>
+{
+    config
+        .ReadFrom.Configuration(context.Configuration)
+        .ReadFrom.Services(provider);
+});
+
+// var logger = new LoggerConfiguration()
+//    .ReadFrom.Configuration(configuration)
+//     .Enrich.FromLogContext()
+//     .CreateLogger();
+//
+// builder.Logging.ClearProviders();
+// builder.Logging.AddSerilog(logger);
+//
+// logger.Information("logging works");
 var app = builder.Build();
 
 // Configure the HTTP request pipeline.
